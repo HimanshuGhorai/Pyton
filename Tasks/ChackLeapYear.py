2324bfb86ca1299af6fdc27580d4e5fe918a7048
himanshu Ghorai <himanshughorai000@gmail.com>
2026-02-14 22:41:37 +0530

    some chenges to the files

--- Tasks/ChackLeapYear.py
+++ Tasks/ChackLeapYear.py
@@ -14,17 +14,7 @@
     
     
     
-# task one => write a pyton program for caulculating tha sum first 10 natural Numbers using loops ?
 
-sum = 0
-
-for i in range(1,11):
-    print(i)
-    
-    sum +=i
-    
-    
-print(sum)
 
 
 
