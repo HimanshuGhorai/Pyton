1f1d656fca4ab224d0721d4985c8feb60c2123d2
himanshu Ghorai <himanshughorai000@gmail.com>
2026-02-14 22:39:59 +0530

    task one complete

---
+++ Tasks/ChackLeapYear.py
@@ -0,0 +1,30 @@
+# year = int(input("Enter the Year: "))
+
+# is_leapYear = False
+
+# if year % 4 == 0:
+#     if year % 100 !=0 or year == 0:
+#         is_leapYear= True
+        
+
+# if is_leapYear:
+#     print(f'{year} is leapYear.')
+# else:
+#     print(f'{year} is not leapYear')
+    
+    
+    
+# task one => write a pyton program for caulculating tha sum first 10 natural Numbers using loops ?
+
+sum = 0
+
+for i in range(1,11):
+    print(i)
+    
+    sum +=i
+    
+    
+print(sum)
+
+
+
